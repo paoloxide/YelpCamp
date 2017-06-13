7ef409e96f93bbc749656844c43fd3668649c05a
Paolo Lambojon <p.t.lambojon@accenture.com>
2017-06-13 21:41:09 +0800

    Added a Jenkinsfile and jenkinsfile-config

---
+++ jenkinsfile-config.groovy
@@ -0,0 +1,7 @@
+docker_CONTAINER_OPTIONS = ' --net "yelpcamp_net" --link yelpcamp_db:mongo '
+ip_DEV_SERVER = "52.213.201.100"
+port_DEV_SERVER_mock = "8184"
+port_DEV_SERVER_develop = "80"
+port_DEV_SERVER_inbound = "3000"
+slack_channel = "hello-node"
+slack_token_id = "slack-token-hello-node"
